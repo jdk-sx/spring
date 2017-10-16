c2ddeda763084bc6751947148b6f704f128bc754
mldn <mldnqa@163.com>
2017-10-16 10:35:18 +0800

    实现单一对象的依赖注入操作

---
+++ mldnspring-di/src/main/java/cn/mldn/mldnspring/di/vo/Emp.java
@@ -0,0 +1,40 @@
+package cn.mldn.mldnspring.di.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@SuppressWarnings("serial")
+public class Emp implements Serializable {
+	private Long empno ;
+	private String ename ;
+	private Double salary ;
+	private Date hiredate ;
+	public Long getEmpno() {
+		return empno;
+	}
+	public void setEmpno(Long empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	public Date getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(Date hiredate) {
+		this.hiredate = hiredate;
+	}
+	@Override
+	public String toString() {
+		return "Emp [empno=" + empno + ", ename=" + ename + ", salary=" + salary + ", hiredate=" + hiredate + "]";
+	}
+}
