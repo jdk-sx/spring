2d80283320ea40ae82c3e8304fd94bf73cbffbd1
mldn <mldnqa@163.com>
2017-10-16 14:13:00 +0800

    自定义初始化与销毁Bean的方法。

--- mldnspring-di/src/main/java/cn/mldn/mldnspring/di/vo/Message.java
+++ mldnspring-di/src/main/java/cn/mldn/mldnspring/di/vo/Message.java
@@ -4,4 +4,10 @@ public class Message {
 	public Message() {
 		System.out.println("【Message类构造方法】timestamp = " + System.currentTimeMillis() + "、this = " + this);
 	}
+	public void initMessage() {
+		System.out.println("【Message初始化-initMessage】建立要进行消息发送的连接通道。");
+	}
+	public void destroyMessage() {
+		System.out.println("【Message销毁-destroyMessage】消息发送完毕，关闭发送通道处理。");
+	}
 }
