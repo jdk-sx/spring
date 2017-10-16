2c686a60585b6ebb64b18a9cfcb72efcebc3d614
mldn <mldnqa@163.com>
2017-10-16 17:05:13 +0800

    ResourceLoader读取资源

--- mldnspring-resource/src/main/java/cn/mldn/mldnspring/resource/ClassPathResourceDemo.java
+++ mldnspring-resource/src/main/java/cn/mldn/mldnspring/resource/ClassPathResourceDemo.java
@@ -2,13 +2,15 @@ package cn.mldn.mldnspring.resource;
 
 import java.util.Scanner;
 
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
  
 public class ClassPathResourceDemo {
 	public static void main(String[] args) throws Exception {
+		ResourceLoader resourceLoader = new DefaultResourceLoader() ;
 		// 创建了一个文件读取的Resource资源对象
-		Resource resource = new ClassPathResource("spring/spring-resource.xml") ;
+		Resource resource = resourceLoader.getResource("classpath:spring/spring-resource.xml") ;
 		System.out.println("资源长度：" + resource.contentLength());
 		// 读取内容可以使用Resource父接口InputStreamSrouce提供的getInputStream()方法来完成
 		Scanner scan = new Scanner(resource.getInputStream()) ;
--- mldnspring-resource/src/main/java/cn/mldn/mldnspring/resource/UrlResourceDemo.java
+++ mldnspring-resource/src/main/java/cn/mldn/mldnspring/resource/UrlResourceDemo.java
@@ -2,13 +2,15 @@ package cn.mldn.mldnspring.resource;
 
 import java.util.Scanner;
 
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.ResourceLoader;
 
 public class UrlResourceDemo {
 	public static void main(String[] args) throws Exception {
-		// 创建了一个文件读取的Resource资源对象
-		Resource resource = new UrlResource("http://localhost/mldn/mldn-data.txt") ;
+		ResourceLoader resourceLoader = new DefaultResourceLoader() ;
+		// 创建了一个文件读取的Resource资源对象 
+		Resource resource = resourceLoader.getResource("http://localhost/mldn/mldn-data.txt") ;
 		System.out.println("资源长度：" + resource.contentLength());
 		// 读取内容可以使用Resource父接口InputStreamSrouce提供的getInputStream()方法来完成
 		Scanner scan = new Scanner(resource.getInputStream()) ;
