df6f2aaad0fccc1f5690811db6ceab291203f677
mldn <mldnqa@163.com>
2017-10-16 15:01:46 +0800

    使用Spring-Test依赖库实现了程序测试。

---
+++ mldnspring-context/src/test/java/cn/mldn/mldnspring/context/TestDeptService.java
@@ -0,0 +1,26 @@
+package cn.mldn.mldnspring.context;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cn.mldn.mldnspring.context.service.IDeptService;
+import cn.mldn.mldnspring.context.vo.Dept;
+import junit.framework.TestCase;
+
+@ContextConfiguration(locations = { "classpath:spring/spring-context.xml" })	// 进行资源文件定位
+@RunWith(SpringJUnit4ClassRunner.class)	// 设置要使用的测试工具
+public class TestDeptService extends TestCase {
+	@Resource
+	private IDeptService deptService ;	// 直接注入业务对象
+	@Test
+	public void testAdd() {
+		Dept dept = new Dept() ;	// 创建VO对象
+		dept.setDeptno(10L);
+		dept.setDname("财务部");
+		TestCase.assertTrue(this.deptService.add(dept));
+	}
+}
