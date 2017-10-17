02f8fafb427994b064b177abb747f3fb61f86c02
mldn <mldnqa@163.com>
2017-10-17 16:03:49 +0800

    实现前置通知的参数接收操作

--- mldnspring-aop/src/main/java/cn/mldn/mldnspring/aop/service/aop/ServiceAdvice.java
+++ mldnspring-aop/src/main/java/cn/mldn/mldnspring/aop/service/aop/ServiceAdvice.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ServiceAdvice { // 随意定义的一个类的名称
-	public void handleBefore() { // 处理前置通知
-		LoggerFactory.getLogger(ServiceAdvice.class).info("【### ServiceAdvice-handleBefore ###】进行业务的前置处理操作。"); // 进行日志输出
+	public void handleBefore(String tempMsg) { // 处理前置通知
+		LoggerFactory.getLogger(ServiceAdvice.class)
+				.info("【### ServiceAdvice-handleBefore ###】进行业务的前置处理操作。接收参数内容为：" + tempMsg); // 进行日志输出
 	}
 
 	public void handleAfter() { // 处理后置操作通知
