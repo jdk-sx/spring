a22b7273a6367477e85ae1dad464e7122115fc82
mldn <mldnqa@163.com>
2017-10-17 08:46:50 +0800

    创建mldnspring-spel项目模块，准备讲解Spring表达式语言。

---
+++ mldnspring-spel/src/main/java/cn/mldn/mldnspring/spel/UrlResourceDemo.java
@@ -0,0 +1,22 @@
+package cn.mldn.mldnspring.spel;
+
+import java.util.Scanner;
+
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+public class UrlResourceDemo {
+	public static void main(String[] args) throws Exception {
+		ResourceLoader resourceLoader = new DefaultResourceLoader() ;
+		// 创建了一个文件读取的Resource资源对象 
+		Resource resource = resourceLoader.getResource("http://localhost/mldn/mldn-data.txt") ;
+		System.out.println("资源长度：" + resource.contentLength());
+		// 读取内容可以使用Resource父接口InputStreamSrouce提供的getInputStream()方法来完成
+		Scanner scan = new Scanner(resource.getInputStream()) ;
+		while (scan.hasNext()) {
+			System.out.print(scan.next()); 
+		}
+		scan.close(); 
+	}
+}
