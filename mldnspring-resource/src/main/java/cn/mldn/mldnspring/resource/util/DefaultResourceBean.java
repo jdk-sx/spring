4fca6da65b88d6227b6797a349f7f2b5cb158c03
mldn <mldnqa@163.com>
2017-10-17 08:44:03 +0800

    资源路径通配符

--- mldnspring-resource/src/main/java/cn/mldn/mldnspring/resource/util/DefaultResourceBean.java
+++ mldnspring-resource/src/main/java/cn/mldn/mldnspring/resource/util/DefaultResourceBean.java
@@ -1,15 +1,13 @@
 package cn.mldn.mldnspring.resource.util;
 
-import java.util.List; 
-
 import org.springframework.core.io.Resource;
 
 public class DefaultResourceBean {
-	private List<Resource> resources ;		// 直接定义要使用的Bean对象
-	public void setResources(List<Resource> resources) {
+	private Resource[] resources ;		// 直接定义要使用的Bean对象
+	public void setResources(Resource[] resources) {
 		this.resources = resources;
 	}
-	public List<Resource> getResources() {
+	public Resource[] getResources() {
 		return resources;
 	}
 }
--- mldnspring-resource/src/test/java/cn/mldn/mldnspring/resource/TestResource.java
+++ mldnspring-resource/src/test/java/cn/mldn/mldnspring/resource/TestResource.java
@@ -1,6 +1,5 @@
 package cn.mldn.mldnspring.resource;
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 import javax.annotation.Resource;
@@ -12,17 +11,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cn.mldn.mldnspring.resource.util.DefaultResourceBean;
 
-@ContextConfiguration(locations = { "classpath:spring/spring-resource.xml" }) // 进行资源文件定位
+@ContextConfiguration(locations = { "classpath:spring/spring-*.xml" }) // 进行资源文件定位
 @RunWith(SpringJUnit4ClassRunner.class) // 设置要使用的测试工具
 public class TestResource {
 	@Resource
 	private DefaultResourceBean resourceBean ;
 	@Test
 	public void testResource() throws Exception { 
-		Iterator<org.springframework.core.io.Resource> iter = this.resourceBean.getResources().iterator() ;
-		while (iter.hasNext()) {
-			Scanner scan = new Scanner(iter.next().getInputStream()) ;
-			scan.useDelimiter("\n") ;
+		for (int x = 0 ; x < this.resourceBean.getResources().length ; x ++) {
+			Scanner scan = new Scanner(this.resourceBean.getResources()[x].getInputStream()) ;
+			scan.useDelimiter("\n") ; 
 			while (scan.hasNext()) {
 				System.out.print(scan.next()); 
 			}
