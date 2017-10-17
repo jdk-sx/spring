ef00dded3d721c8af23baaa9da6c8d7473252ca6
mldn <mldnqa@163.com>
2017-10-17 16:07:58 +0800

    AOP后置返回通知

--- mldnspring-aop/src/main/java/cn/mldn/mldnspring/aop/service/aop/ServiceAdvice.java
+++ mldnspring-aop/src/main/java/cn/mldn/mldnspring/aop/service/aop/ServiceAdvice.java
@@ -7,10 +7,15 @@ import org.springframework.stereotype.Component;
 public class ServiceAdvice { // 随意定义的一个类的名称
 	public void handleBefore(String tempMsg) { // 处理前置通知
 		LoggerFactory.getLogger(ServiceAdvice.class)
-				.info("【### ServiceAdvice-handleBefore ###】进行业务的前置处理操作。接收参数内容为：" + tempMsg); // 进行日志输出
+				.info("【### 1、ServiceAdvice-handleBefore ###】进行业务的前置处理操作。接收参数内容为：" + tempMsg); // 进行日志输出
+	}
+	
+	public void handleReturn(String retMsg) { // 处理返回通知
+		LoggerFactory.getLogger(ServiceAdvice.class)
+				.info("【### 3、ServiceAdvice-handleReturn ###】业务方法执行完毕：" + retMsg); // 进行日志输出
 	}
 
 	public void handleAfter() { // 处理后置操作通知
-		LoggerFactory.getLogger(ServiceAdvice.class).info("【### ServiceAdvice-handleAfter ###】进行业务的后置处理操作。"); // 进行日志输出
+		LoggerFactory.getLogger(ServiceAdvice.class).info("【### 2、ServiceAdvice-handleAfter ###】进行业务的后置处理操作。"); // 进行日志输出
 	}
 }
