52305cb1c806c0aad37f3d8eb9898c458108c656
mldn <mldnqa@163.com>
2017-10-17 09:13:00 +0800

    SpEL语法分析。

---
+++ mldnspring-spel/src/main/java/cn/mldn/mldnspring/spel/TestSpelDemo04.java
@@ -0,0 +1,18 @@
+package cn.mldn.mldnspring.spel;
+
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+public class TestSpelDemo04 {
+	public static void main(String[] args) {
+		String str = "1.1 + 2.2" ;	// 将之前的语句都定义在了字符串里面
+		ExpressionParser parser = new SpelExpressionParser() ;	// 定义一个SpEL的语法解析器
+		Expression exp = parser.parseExpression(str) ;	// 将字符串解析为一个正常的表达式对象
+		EvaluationContext context = new StandardEvaluationContext(exp) ;// 实现表达式的具体计算处理
+		Object value = exp.getValue(context) ;
+		System.out.println(value); 
+	}
+}
