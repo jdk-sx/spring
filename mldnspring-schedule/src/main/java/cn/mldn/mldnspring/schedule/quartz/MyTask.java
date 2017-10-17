9999a1328118f695b14cbe6fbbf770d2f1a3be50
mldn <mldnqa@163.com>
2017-10-17 13:56:28 +0800

    基于配置实现QuartZ任务组件调用。

--- mldnspring-schedule/src/main/java/cn/mldn/mldnspring/schedule/quartz/MyTask.java
+++ mldnspring-schedule/src/main/java/cn/mldn/mldnspring/schedule/quartz/MyTask.java
@@ -2,14 +2,9 @@ package cn.mldn.mldnspring.schedule.quartz;
 
 import java.text.SimpleDateFormat;
 
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.scheduling.quartz.QuartzJobBean;
 
-public class MyTask extends QuartzJobBean { // 此时就定义好了一个专门负责任务处理的程序类
-
-	@Override
-	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+public class MyTask {
+	protected void runTask() {	// 自己随意定义了一个方法名称
 		System.out.println("【当前的日期时间】" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date()));
 	}
 
