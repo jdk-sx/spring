3f112b73714c4c4a32d1b7fc931749eedc787237
mldn <mldnqa@163.com>
2017-10-19 10:20:35 +0800

    分析“@RequestParam”注解的使用，但是一般不建议出现。

--- mldnspring-springmvc/src/main/java/cn/mldn/mldnspring/springmvc/action/EchoAction.java
+++ mldnspring-springmvc/src/main/java/cn/mldn/mldnspring/springmvc/action/EchoAction.java
@@ -5,25 +5,30 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller	// 所有需要被Spring管理的程序类一定要进行配置
+@Controller // 所有需要被Spring管理的程序类一定要进行配置
 @RequestMapping("/pages/message/*") // 与方法中的路径做结合
-public class EchoAction {	// 定义了一个自己的处理程序类
-	private Logger log = LoggerFactory.getLogger(EchoAction.class) ;
+public class EchoAction { // 定义了一个自己的处理程序类
+	private Logger log = LoggerFactory.getLogger(EchoAction.class);
+
 	@RequestMapping("echo_pre")
 	public String echoPre() {
-		return "/pages/message/message_input.jsp" ;
-	} 
-	
-	
-	
-	@RequestMapping(path="echo",method=RequestMethod.POST)	// 以后访问此方法的路径就是“echo.action”； 
-	public ModelAndView echo(String msg) {	// 该参数会根据参数名称自动进行匹配处理
+		return "/pages/message/message_input.jsp";
+	}
+
+	@RequestMapping("echo") // 以后访问此方法的路径就是“echo.action”；
+	public ModelAndView echo(
+			@RequestParam(	// 表示对请求参数的配置
+					name = "msg", 	// 表示映射的请求参数的名称
+					required = false, 	// 表示该参数是否必须传递
+					defaultValue = "HelloWorld"	// 设置该参数为null时的默认值
+			) String msg) { // 该参数会根据参数名称自动进行匹配处理
 		this.log.info("*** EchoAction接收到请求参数，msg = " + msg);
 		// ModelAndView主要功能是设置跳转路径以及进行request属性的保存
-		ModelAndView mav = new ModelAndView("/pages/message/message_show.jsp") ;
-		mav.addObject("echoMessage", "【ECHO】msg = " + msg) ;	// 设置request属性范围
-		return mav ;
+		ModelAndView mav = new ModelAndView("/pages/message/message_show.jsp");
+		mav.addObject("echoMessage", "【ECHO】msg = " + msg); // 设置request属性范围
+		return mav;
 	}
 }
