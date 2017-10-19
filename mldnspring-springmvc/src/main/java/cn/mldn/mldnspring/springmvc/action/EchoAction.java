a6d5309198034a0b91522f18497bb0bb4d60d3e8
mldn <mldnqa@163.com>
2017-10-19 11:30:49 +0800

    配置JSP页面的安全访问处理。

--- mldnspring-springmvc/src/main/java/cn/mldn/mldnspring/springmvc/action/EchoAction.java
+++ mldnspring-springmvc/src/main/java/cn/mldn/mldnspring/springmvc/action/EchoAction.java
@@ -6,8 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller // 所有需要被Spring管理的程序类一定要进行配置
@@ -17,16 +15,16 @@ public class EchoAction { // 定义了一个自己的处理程序类
 
 	@RequestMapping("echo_pre")
 	public String echoPre() {
-		return "/pages/message/message_input.jsp";
+		return "message/message_input";
 	}
 
 	@RequestMapping("echo") // 以后访问此方法的路径就是“echo.action”；
 	public ModelAndView echo(String msg, String tags[]) { // 该参数会根据参数名称自动进行匹配处理
 		this.log.info("*** EchoAction接收到请求参数，msg = " + msg + "、标签：" + Arrays.toString(tags)); 
 		// ModelAndView主要功能是设置跳转路径以及进行request属性的保存
-		ModelAndView mav = new ModelAndView("/pages/message/message_show.jsp");
+		ModelAndView mav = new ModelAndView("message/message_show");
 		mav.addObject("echoMessage", "【ECHO】msg = " + msg); // 设置request属性范围
 		mav.addObject("echoTags", Arrays.toString(tags)); // 设置request属性范围
 		return mav;
-	}
+	} 
 }
